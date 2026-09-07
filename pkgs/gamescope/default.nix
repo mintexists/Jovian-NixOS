835da3e9896096bb4c418e5f5b2a234acdd3e1fa
mint <60362123+mintexists@users.noreply.github.com>
2026-09-07 13:06:29 -0400

    Switch gamescope source to OpenGamingCollective
    Updated gamescope to use OpenGamingCollective repository and changed version to 3.16.19.

--- pkgs/gamescope/default.nix
+++ pkgs/gamescope/default.nix
@@ -6,14 +6,14 @@
 # NOTE: vendoring gamescope for the time being since we want to match the
 #       version shipped by the vendor, ensuring feature level is equivalent.
 
-gamescope'.overrideAttrs(old: rec {
-  version = "3.16.26";
-
-  src = fetchFromGitHub {
-    owner = "ValveSoftware";
-    repo = "gamescope";
-    rev = version;
-    fetchSubmodules = true;
-    hash = "sha256-6B9Vbr21EHDOVCx27TZk3ruJPqoovEQ1JYtd8F6Siv4=";
-  };
+gamescope'.overrideAttrs (old: {
+        enableWsi = true;
+        src = prev.fetchFromGitHub {
+          owner = "OpenGamingCollective";
+          repo = "gamescope";
+          tag = "3.16.19";
+          fetchSubmodules = true;
+          hash = "sha256-6B9Vbr21EHDOVCx27TZk3ruJPqoovEQ1JYtd8F6Siv4=";
+        };
+      });
 })
